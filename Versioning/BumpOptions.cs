233949774b2d49be205c593692a8ffcadae533ab
Tim <hounddog@gmail.com>
2022-08-02 06:30:21 -0400

    patch 1.0.1

--- Versioning/BumpOptions.cs
+++ Versioning/BumpOptions.cs
@@ -1,10 +1,20 @@
 ﻿using CommandLine;
 
-[Verb("bump", aliases:new []{"b"})]
+[Verb("bump-package", aliases:new []{"b"})]
 public class BumpOptions
 {
+
+    [Option('p',"patch",HelpText = "Bump The Patch Version")]
+    public bool Patch { get; set; }
     public async Task ApplyAsync()
     {
-        Console.WriteLine("You Want To Bump");
+        if (this.Patch)
+        {
+            Console.WriteLine("You want to bump the patch");
+        }
+        else
+        {
+            Console.WriteLine("You DO NOT want to bump the patch");
+        }
     }
 }
